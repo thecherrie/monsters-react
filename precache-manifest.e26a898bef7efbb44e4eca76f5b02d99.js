c7aa1dddb82877505b54a58a153b933e3ae76814
Ash <asherchez@gmail.com>
2020-09-13 17:23:57 +0100

    Updates

---
+++ precache-manifest.e26a898bef7efbb44e4eca76f5b02d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ce36a1f0bd58a9a0996b8fbd6f8e30",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "968de79c70cd8c9cbf74",
+    "url": "/monsters-react/static/css/main.07b5a1c8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968de79c70cd8c9cbf74",
+    "url": "/monsters-react/static/js/main.937ebf53.chunk.js"
+  },
+  {
+    "revision": "52d0e7f35021692ce0ba",
+    "url": "/monsters-react/static/js/runtime-main.2e36a746.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
